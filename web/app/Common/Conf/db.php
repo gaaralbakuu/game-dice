28d270bf06933ef232ffa7d17f62885e1ed2417d
Gaaralbakuu <Dat-PhamMinh@vn.apachefootwear.com>
2023-10-24 10:53:49 +0700

    modify

--- web/app/Common/Conf/db.php
+++ web/app/Common/Conf/db.php
@@ -3,8 +3,8 @@ return [
 	'DB_TYPE'               => 'mysql',
     'DB_HOST'               => '127.0.0.1',
     'DB_NAME'               => '123',
-    'DB_USER'               => 'root',
-    'DB_PWD'                => '',  //蜜桃源码网--www.884347.com
+    'DB_USER'               => 'root123',
+    'DB_PWD'                => '123',  //蜜桃源码网--www.884347.com
     'DB_PREFIX'             => 'caipiao_',
     'DB_PORT'               => '3306',
 	'DB_DEBUG'              => false,
